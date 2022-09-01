b41e8c3a70c534a89a2eedfcc10a2475bcf5a0b1
Yongjoo Yoo <yjoo.eyoo@gmail.com>
2022-09-01 14:12:37 -0700

    SWC-6272: add CertificationQuiz into index

--- src/lib/umd.index.ts
+++ src/lib/umd.index.ts
@@ -40,6 +40,7 @@ import TermsAndConditions from './containers/TermsAndConditions'
 import { displayToast, SynapseToastContainer } from './containers/ToastMessage'
 import { TrashCanList } from './containers/trash/TrashCanList'
 import { OAuthManagement } from './containers/oauth/OAuthManagement'
+import CertificationQuiz from './containers/CertificationQuiz'
 import UserCard from './containers/UserCard'
 import UserProfileLinks from './containers/user_profile_links/UserProfileLinks'
 import { ProvenanceGraph } from './containers/provenance/ProvenanceGraph'
@@ -103,6 +104,7 @@ const SynapseComponents = {
   ProvenanceGraph,
   TrashCanList,
   OAuthManagement,
+  CertificationQuiz,
 }
 
 // Include the version in the build
